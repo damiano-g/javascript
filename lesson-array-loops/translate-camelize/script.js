3623d0f68bc4478a28b306a0055f21efa4d2023a
Damiano <114597691+damiano-g@users.noreply.github.com>
2025-08-07 11:33:57 +0200

    array.length synthax fixed

--- lesson-array-loops/translate-camelize/script.js
+++ lesson-array-loops/translate-camelize/script.js
@@ -4,7 +4,7 @@ let myArray = myString.split('-');
 
 function camelize(str){
 
-    for(let i=1; i<myArray.length(); i++){
+    for(let i=1; i < myArray.length; i++){
         let newString = myArray[i].replace(myArray[i].charAt(0), myArray[i].charAt(0).toUpperCase());
         myArray[i] = newString;
     }
