84e8577aa993ca506c26e27262b1c617a2e09769
Damiano <114597691+damiano-g@users.noreply.github.com>
2025-08-08 10:20:10 +0200

    New paragraph created and added at the end of section

--- lesson-dom/script.js
+++ lesson-dom/script.js
@@ -1,6 +1,11 @@
 const link = document.querySelector("a");
 const sect = document.querySelector("section");
+const para = document.createElement("p");
 
 link.textContent = "Mozilla Developer Network";
 
 link.href = "https://developer.mozzila.org";
+
+para.textContent = "We hope you enjoyed the ride.";
+
+sect.appendChild(para);
