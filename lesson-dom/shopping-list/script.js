6ec5c6e7aa0b53ee2c93ced25d0f02e86d9712cb
Damiano <114597691+damiano-g@users.noreply.github.com>
2025-08-08 11:36:40 +0200

    Create list item on button click func

--- lesson-dom/shopping-list/script.js
+++ lesson-dom/shopping-list/script.js
@@ -1,3 +1,22 @@
 const list = document.querySelector("ul");
 const inElem = document.querySelector("input");
 const but = document.querySelector("button");
+
+function buttonPressed(){
+    let inputValue = inElem.value();
+    inElem.value() = "";
+
+    const listElem = document.createElement("li");
+    const text = document.createElement("span");
+    const delBut = document.createElement("button");
+
+    listElem.appendChild(text);
+    listElem.appendChild(delBut);
+
+    text.textContent(inputValue);
+    delBut.textContent("Delete");
+
+    list.appendChild(listElem);
+}
+
+but.addEventListener("click", buttonPressed());
